8b0c7daf0e824604318c22755689b3563ebb3b3d
Nihat <kaffinpx@gmail.com>
2021-08-25 21:32:47 +0300

    Update Web3.js

--- src/Web3.js
+++ src/Web3.js
@@ -57,15 +57,22 @@ module.exports = class Web3 extends EventEmitter {
           const latestHash = hash.toString().split(':')[1]
           const calculateHash = crypto.createHash('ripemd160').update(createdAt + latestHash + wallet.KWallet + receiver + amount).digest('hex')
           curve.sign(calculateHash, privKey).then(tx => {
-            resolve({ signature: tx, data: `${createdAt}|${wallet.KWallet}|${wallet.publicKey}|${receiver}|${amount}|${tx}` })
+            resolve({ signature: tx, hash: calculateHash, data: `${createdAt}|${wallet.KWallet}|${wallet.publicKey}|${receiver}|${amount}|${tx}` })
           })
         })
       })
     })
   }
 
-  broadcastTransactionBlock (data) {
+  bcTransactionBlock (data) {
     if (!this.status === 'READY') throw new Error('NOT_CONNECTED')
-    this.ws.send(`newRawTransaction:${data}`)
+    return new Promise((resolve, reject) => {
+      this.ws.send(`newRawTransaction:${data}`)
+      waitForData(this.ws, 'queuedSuccess').then(data => {
+        resolve()
+      }).catch(err => {
+        reject(err)
+      })
+    })
   }
 }
