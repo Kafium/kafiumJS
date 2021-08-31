c0ee440552696607d4bd23901363db1b09a0b1a1
Nihat <kaffinpx@gmail.com>
2021-08-31 23:01:46 +0300

    Fix KPoW + Add getTotalBlocks() Function

--- src/Web3.js
+++ src/Web3.js
@@ -33,6 +33,19 @@ module.exports = class Web3 extends EventEmitter {
     })
   }
 
+  getTotalBlocks () {
+    if (!this.status === 'READY') throw new Error('NOT_CONNECTED')
+    return new Promise((resolve, reject) => {
+      this.ws.send(`getBlocksCount`)
+
+      waitForData(this.ws, 'blocksCount').then(balance => {
+        resolve(parseInt(balance.split(':')[1]))
+      }).catch(err => {
+        reject(err)
+      })
+    })
+  }
+
   getLastHash (KWallet) {
     if (!this.status === 'READY') throw new Error('NOT_CONNECTED')
     return new Promise((resolve, reject) => {
@@ -57,7 +70,7 @@ module.exports = class Web3 extends EventEmitter {
           const createdAt = Date.now()
           const latestHash = hash.toString().split(':')[1]
           const calculateHash = crypto.createHash('ripemd160').update(createdAt + latestHash + wallet.KWallet + receiver + amount).digest('hex')
-          const work = KPoW.getWork(calculateHash)
+          const work = KPoW.doWork(calculateHash)
           curve.sign(calculateHash, privKey).then(tx => {
             resolve({ signature: tx, hash: calculateHash, data: `${createdAt}|${wallet.KWallet}|${wallet.publicKey}|${receiver}|${amount}|${work}|${tx}` })
           })
