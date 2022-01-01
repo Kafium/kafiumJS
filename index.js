126094d841c4d132030f46aa17d4c0b64eea2f24
Nihat <kaffinpx@gmail.com>
2022-01-02 02:59:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const block = require('./src/block')
+const node = require('./src/node')
 const wallet = require('./src/wallet')
 
+module.exports.block = block
+module.exports.node = node
 module.exports.wallet = wallet
